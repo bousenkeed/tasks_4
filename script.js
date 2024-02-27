e91e520e7fcf12cca9d62900a2eaba58c88c6ad8
bousenkeed <ant.sakh38@gmail.com>
2024-02-27 20:00:33 +0300

    2th

--- script.js
+++ script.js
@@ -1,98 +1,59 @@
 
-//                           Задача: Управление библиотекой
+// //                           Задача: Управление библиотекой
 
 const books = [
     { id: 1, title: "Война и мир", author: "Лев Толстой", isAvailable: true },
     { id: 2, title: "Гарри Поттер и философский камень", author: "Дж. К. Роулинг", isAvailable: false },
-    { id: 3, title: "Улисс", author: "Джеймс Джойс", isAvailable: true }
+    { id: 3, title: "Улисс", author: "Джеймс Джойс", isAvailable: true },
+    { id: 4, title: "Анна Каренина", author: "Лев Толстой", isAvailable: true }
 ];
 const requests = [
     { userId: 101, bookId: 1 },
     { userId: 102, bookId: 2 },
-    { userId: 103, bookId: 3 }
+    { userId: 103, bookId: 3 },
+    { userId: 104, bookId: 4 }
 ];
 
-//1 - Проверка наличия книги
-function isHasBook(bookId) {
+// 1: Проверка доступности книги:
 
-    for (i = 0; i < books.length; i++) {
-        if (books[i].id === bookId) {
-            return books[i].isAvailable;
-        }
-    }
-}
-console.log(isHasBook(1));
+function isBookAvailable(books, bookId) {
+    const findedBook = books.find(book => book.id === bookId)
+    return findedBook ? findedBook.isAvailable : false;
+};
+console.log(isBookAvailable(books, 4));
 
-//2 - Поиск книги по автору
-function findBooksByAuthor(author) {
-    let authorBooks = [];
-    for (i = 0; i < books.length; i++) {
-        if (books[i].author === author) {
-            authorBooks.push(books[i].title);
-            return authorBooks;
-        }
-    }
+// 2: Поиск книги по автору: 
+
+function findBooksByAuthor(books, author) {
+    return books
+        .filter(book => book.author === author && book.isAvailable)
+        .map(book => book.title)
 }
 
-console.log(findBooksByAuthor("Лев Толстой"));
+console.log(findBooksByAuthor(books, 'Лев Толстой'));
+// 3: Обработка запросов на выдачу книг:
 
-//3 - Обработка запросов на выдачу книг
 function processRequests(books, requests) {
-    for (i = 0; i < requests.length; i++) {
-        if (isHasBook(requests[i].bookId)) {
-            books[i].isAvailable = false;
-            console.log(`Книга выдана пользователю: Id${requests[i].userId}`)
+    requests.forEach(request => {
+        const newBook = books.find(book => request.bookId === book.id);
+        if (newBook && newBook.isAvailable) {
+            newBook.isAvailable = false;
+            console.log(`Книга выдана пользователю`)
         } else {
-            console.log(`Книга ${books[i].title} автора ${books[i].author} - уже выдана!`)
+            console.log('Занято');
         }
-    }
-}
-
+    })
+};
 processRequests(books, requests);
 
-//4 - Возврат книги
-function returnBook(id) {
-    for (i = 0; i < books.length; i++) {
-        if (books[i].id === id) {
-            books[i].isAvailable = true;
-        }
+// 4: Возврат книги:
+
+function returnBook (books, bookId) {
+    const availableBook = books.find(book => book.id === bookId);
+    if (availableBook) {
+        availableBook.isAvailable = true;
     }
 }
+returnBook(books, 3);
 
-returnBook(1); //возвращаем первую книгу
-console.log(isHasBook(1));
-
-
-
-
-
-
-//                     Задача: Система управления заказами в ресторане
-
-const ingredients = [
-    { id: 1, name: "Томаты", quantity: 10 },
-    { id: 2, name: "Моцарелла", quantity: 5 },
-    { id: 3, name: "Базилик", quantity: 8 }
-];
-
-const dishes = [
-    { id: 1, name: "Маргарита", ingredients: [1, 2, 3] },
-];
-
-const orders = [
-    { orderId: 1, dishId: 1, status: "новый" }
-];
-
-// 1 Проверка возможности приготовления блюда
-
-function checkDishes(dishId) {
-    let isHasIngredient = true;
-    for (let i = 0; i < dishes[dishId - 1].ingredients.length; i++) {
-        if (ingredients[dishes[dishId - 1].ingredients[i] - 1].quantity <= 0) {
-            isHasIngredient = false;
-        }
-    }
-    return isHasIngredient;
-}
-
-console.log(checkDishes(1));
+console.log(isBookAvailable(books, 3));
